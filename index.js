42e119b3548e4a2ff8797d2aff44ec96e9cd842b
FunFoxRR <dldilley01@outlook.com>
2023-01-28 14:51:16 -0500

    Update index.html

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ if (localStorage.getItem("isLoggedIn") === "true") {
     signUpNowButton.disabled = true;
     signUpNowButton.parentElement.href = "#";
 }
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i=0; i<reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+        //else {
+        //     reveals[i].classList.remove("active");
+        // }
+    }
+}
+
+window.addEventListener('scroll', reveal);
