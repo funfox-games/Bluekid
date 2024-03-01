84acc6cd7a04610788e9f7182ea65c031600fcc7
FunFoxRR <dldilley01@outlook.com>
2024-03-01 14:49:55 -0500

    test backdrop-flter

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function onLoad() {
     alert("LOAD");
-    document.getElementById("topnav_blur").style.height = document.getElementById("topnav").clientHeight + " px";
+    // document.getElementById("topnav_blur").style.height = document.getElementById("topnav").clientHeight + " px";
 }
