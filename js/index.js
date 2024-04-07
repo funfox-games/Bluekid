1356b39c6bdb7fdd90e53bf77ce6cfd19efcbf1a
FunFoxRR <dldilley01@outlook.com>
2024-04-06 20:20:16 -0400

    Update home page, add some assets

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-function onLoad() {
-    alert("LOAD");
-    // document.getElementById("topnav_blur").style.height = document.getElementById("topnav").clientHeight + " px";
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round($elem.offset().top);
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
 }
+window.addEventListener("scroll", () => {
+    for (let i = 0; i < document.getElementsByClassName("onscroll").length; i++) {
+        const element = document.getElementsByClassName("onscroll")[i];
+        if (isElementInViewport(element)) {
+            element.classList.add("animate__animated");
+        } else {
+            element.classList.remove("animate__animated");
+        }
+    }
+})
