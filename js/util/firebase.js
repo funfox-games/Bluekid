37590df42708c9b9d2ad19821df432d2989b3d65
FunFoxRR <dldilley01@gmail.com>
2024-12-16 17:00:54 -0500

    Update firebase.js

--- js/util/firebase.js
+++ js/util/firebase.js
@@ -1,4 +1,216 @@
-import{initializeApp}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";import{FIREBASE_API_KEY,FIREBASE_APP_ID}from"../../config.js";let firebaseConfig={apiKey:FIREBASE_API_KEY,authDomain:"bluekid-303db.firebaseapp.com",databaseURL:"https://bluekid-303db-default-rtdb.firebaseio.com",projectId:"bluekid-303db",storageBucket:"bluekid-303db.appspot.com",messagingSenderId:"207140973406",appId:FIREBASE_APP_ID},app=initializeApp(firebaseConfig);import{getAuth,signInAnonymously,reauthenticateWithPopup,onAuthStateChanged,EmailAuthProvider,signOut,sendPasswordResetEmail,deleteUser,updateEmail,GoogleAuthProvider,signInWithPopup,signInWithEmailAndPassword,reauthenticateWithCredential}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";let auth=getAuth();import{getFirestore,Timestamp,doc,getDoc,getDocs,updateDoc,deleteDoc,collection,query,where,limit,orderBy,setDoc,addDoc,startAfter}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";let db=getFirestore(app);import{getDatabase,ref,set as realtimeSet,update as realtimeUpdate,get as realtimeGet,remove as realtimeRemove,onDisconnect,onValue}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";let realtime=getDatabase(app);import{getAnalytics}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";let analytics=getAnalytics(app);import{getStorage,ref as storageref,uploadBytes,getDownloadURL,deleteObject,listAll}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";let storage=getStorage();import{getFunctions,httpsCallable}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-functions.js";let functions=getFunctions(app),ONLINE_TEXT="Online",OFFLINE_TEXT="Offline",KIT_COVER_LOCATION="kit/cover",currentDisconnect,shouldBeChecked=!0,DEVELOPER_ALLOW_LIST=["SfgsyTdWadMcWNkoKzMcr5vZANz1","IRQFJKtbT0UKx9OfrhGEsj557bB2"],MODERATOR_ALLOW_LIST=["SfgsyTdWadMcWNkoKzMcr5vZANz1","IRQFJKtbT0UKx9OfrhGEsj557bB2","L9DshmPRcQYemY4E0tlHJqk5X053"],adjectiveWords=["Blue","Long","Important","Stupendous","Avaricous","Arrogant","Whimsical","Wacky","Attractive","Rapacious","Preposterous","Luscious","Red","Purple","Orange"],animals=["Dog","Cat","Cow","Frog","Jellyfish","Lizard","Llama","Turtle","Gorilla","Fox","Wolf","Eel","Hawk","Rat","Snake","Peacock","Bear","Racoon","Duck","Kangaroo"],_hasBKP=null;function forceOffline(){return new Promise(async(e,t)=>{null!=currentDisconnect&&(currentDisconnect.cancel(),shouldBeChecked=!1);var a=ref(realtime,"statuses/"+auth.currentUser.uid);await realtimeUpdate(a,{status:OFFLINE_TEXT,lastOnline:new Date}).catch(e=>{console.error(e),t(e)}),e()})}let hasBluekidPlus=async()=>{var e;return null==_hasBKP&&(e=httpsCallable(functions,"hasBluekidPlus"),_hasBKP=(_hasBKP=await e(auth.currentUser.uid)).data.active),_hasBKP},filterMessage=async t=>{await(await fetch("../../asset/no-nos.txt")).text();var a=t.split(" ");t="";for(let e=0;e<a.length;e++)t+=a[e]+" ";return t.trim(),t};document.body.onload=()=>{"false"==localStorage.getItem("settings-allowThemes")&&document.body.setAttribute("disableThemes","true")},onAuthStateChanged(auth,async e=>{if(e&&!e.isAnonymous){let t=ref(realtime,"statuses/"+e.uid);if(console.log("alr"),onValue(t,e=>{(currentDisconnect=onDisconnect(t)).update({status:OFFLINE_TEXT,lastOnline:new Date}).then(function(){shouldBeChecked&&realtimeUpdate(t,{status:ONLINE_TEXT})})}),await hasBluekidPlus()){var a=document.getElementsByClassName("subscription_btn");for(let e=0;e<a.length;e++)a[e].style.display="none"}}});class FirebaseHelper{static async getUserStatus(s){return new Promise(async(e,t)=>{var a=await realtimeGet(ref(realtime,"statuses/"+s)).catch(e=>{console.log(e),t(e)});a.exists()&&null!=a.val().status?e({status:a.val().status,lastOnline:a.val().lastOnline}):e({status:OFFLINE_TEXT,lastOnline:null})})}static async getUserMetadata(a){return new Promise(async(e,t)=>{e((await fetch("https://bluekidapi.netlify.app/.netlify/functions/api/user/profile/",{headers:{user:a,"Content-Type":"application/json"}}).then(async e=>e.json())).data)})}static async getUserData(a){return new Promise(async(e,t)=>{e((await getDoc(doc(db,"users",a)).catch(e=>{console.log(e),t(e)})).data())})}static async getBadges(a){return new Promise(async(e,t)=>{e((await httpsCallable(functions,"getBadges")(a)).data)})}}let HOSTING_VERSION="1.0.0-alpha.1",firefoxHtml=`
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { FIREBASE_API_KEY, FIREBASE_APP_ID } from "https://bluekid.netlify.app/config.js";
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: "bluekid-303db.firebaseapp.com",
+    databaseURL: "https://bluekid-303db-default-rtdb.firebaseio.com",
+    projectId: "bluekid-303db",
+    storageBucket: "bluekid-303db.appspot.com",
+    messagingSenderId: "207140973406",
+    appId: FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getAuth, signInAnonymously, reauthenticateWithPopup, onAuthStateChanged, EmailAuthProvider, signOut, sendPasswordResetEmail, deleteUser, updateEmail, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, reauthenticateWithCredential } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+export const auth = getAuth();
+
+import { getFirestore, Timestamp, doc, getDoc, getDocs, updateDoc, deleteDoc, collection, query, where, limit, orderBy, setDoc, addDoc, startAfter } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+export const db = getFirestore(app);
+
+import { getDatabase, ref, set as realtimeSet, update as realtimeUpdate, get as realtimeGet, remove as realtimeRemove, onDisconnect, onValue } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+export const realtime = getDatabase(app);
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+export const analytics = getAnalytics(app);
+
+import { getStorage, ref as storageref, uploadBytes, getDownloadURL, deleteObject, listAll } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+export const storage = getStorage();
+
+import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-functions.js";
+const functions = getFunctions(app);
+
+export const ONLINE_TEXT = "Online";
+export const OFFLINE_TEXT = "Offline";
+
+export const KIT_COVER_LOCATION = "kit/cover";
+
+let currentDisconnect;
+let shouldBeChecked = true;
+
+export const DEVELOPER_ALLOW_LIST = ["SfgsyTdWadMcWNkoKzMcr5vZANz1", "IRQFJKtbT0UKx9OfrhGEsj557bB2"];
+export const MODERATOR_ALLOW_LIST = ["SfgsyTdWadMcWNkoKzMcr5vZANz1", "IRQFJKtbT0UKx9OfrhGEsj557bB2", "L9DshmPRcQYemY4E0tlHJqk5X053"];
+
+export const adjectiveWords = [
+    "Blue",
+    "Long",
+    "Important",
+    "Stupendous",
+    "Avaricous",
+    "Arrogant",
+    "Whimsical",
+    "Wacky",
+    "Attractive",
+    "Rapacious",
+    "Preposterous",
+    "Luscious",
+    "Red",
+    "Purple",
+    "Orange"
+]
+export const animals = [
+    "Dog",
+    "Cat",
+    "Cow",
+    "Frog",
+    "Jellyfish",
+    "Lizard",
+    "Llama",
+    "Turtle",
+    "Gorilla",
+    "Fox",
+    "Wolf",
+    "Eel",
+    "Hawk",
+    "Rat",
+    "Snake",
+    "Peacock",
+    "Bear",
+    "Racoon",
+    "Duck",
+    "Kangaroo"
+]
+
+let _hasBKP = null;
+
+export function forceOffline() {
+    return new Promise(async (res, rej) => {
+        if (currentDisconnect != null) {
+            currentDisconnect.cancel();
+            shouldBeChecked = false;
+        }
+        const statusref = ref(realtime, "statuses/" + auth.currentUser.uid);
+        await realtimeUpdate(statusref, { status: OFFLINE_TEXT, lastOnline: new Date() }).catch((err) => {
+            console.error(err);
+            rej(err);
+        });
+        res();
+    });
+}
+
+const hasBluekidPlus = async () => {
+    if (_hasBKP == null) {
+        const func = httpsCallable(functions, "hasBluekidPlus");
+        _hasBKP = await func(auth.currentUser.uid);
+        _hasBKP = _hasBKP.data.active;
+    }
+    return _hasBKP;
+}
+
+const filterMessage = async (msg) => {
+    const __content = await fetch("../../asset/no-nos.txt");
+    const _content = await __content.text();
+
+    const splitmsg = msg.split(" ");
+    msg = "";
+    for (let i = 0; i < splitmsg.length; i++) {
+        let word = splitmsg[i];
+        // if (_content.includes(word)) {
+        //     word = word.length;
+        // }
+        msg += word + " ";
+    }
+    msg.trim();
+    return msg;
+}
+
+document.body.onload = () => {
+    if (localStorage.getItem("settings-allowThemes") == "false") {
+        document.body.setAttribute("disableThemes", "true");
+    }
+}
+
+onAuthStateChanged(auth, async (user) => {
+    if (user && !user.isAnonymous) {
+        const statusref = ref(realtime, "statuses/" + user.uid);
+        console.log("alr");
+        onValue(statusref, (snapshot) => {
+            // if (snapshot.exists() == false) {
+            //     return;
+            // };
+            currentDisconnect = onDisconnect(statusref)
+            currentDisconnect.update({ status: OFFLINE_TEXT, lastOnline: new Date() }).then(function () {
+                if (!shouldBeChecked) {return;}
+                realtimeUpdate(statusref, { status: ONLINE_TEXT });
+            });
+        });
+
+        const hasBKP = await hasBluekidPlus();
+        if (hasBKP) {
+            const elems = document.getElementsByClassName("subscription_btn");
+            for (let i = 0; i < elems.length; i++) {
+                const elem = elems[i];
+                elem.style.display = "none";
+            }   
+        }
+
+        // console.log(await filterMessage("hello users, im going to cream"));
+    }
+});
+
+export class FirebaseHelper {
+    static async getUserStatus(uid) {
+        return new Promise(async (res, rej) => {
+            const result = await realtimeGet(ref(realtime, "statuses/" + uid)).catch((err) => {
+                console.log(err);
+                rej(err);
+            });
+
+            if (!result.exists() || result.val().status == null) {
+                res({ status: OFFLINE_TEXT, lastOnline: null });
+                return;
+            }
+            res({status: result.val().status, lastOnline: result.val().lastOnline});
+        });
+    }
+    static async getUserMetadata(uid) {
+        return new Promise(async (res, rej) => {
+            const result = await fetch("https://bluekidapi.netlify.app/.netlify/functions/api/user/profile/", {
+                "headers": {
+                    "user": uid,
+                    "Content-Type": "application/json"
+                }
+            }).then(async (val) => {
+                return val.json();
+            });
+            res(result.data);
+        });
+        
+    }
+    static async getUserData(uid) {
+        return new Promise(async (res, rej) => {
+            const result = await getDoc(doc(db, "users", uid)).catch((err) => {
+                console.log(err);
+                rej(err);
+            });
+            res(result.data());
+        });
+    }
+
+    static async getBadges(uid) {
+        return new Promise(async (res, rej) => {
+            const func = httpsCallable(functions, "getBadges");
+            const result = await func(uid);
+
+            res(result.data);
+        });
+    }
+}
+
+const HOSTING_VERSION = "1.0.0-alpha.1";
+
+const firefoxHtml = `
     <div id="firefoxWarningthing">
         <i class="fa-light fa-triangle-exclamation"></i>
     </div>
@@ -9,4 +221,17 @@ import{initializeApp}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-app
             <button class="puffy_button primary">Got it</button>
         </form>
     </dialog>
-`,userAgentString=navigator.userAgent,firefoxAgent=-1<userAgentString.indexOf("Firefox");firefoxAgent&&(document.body.innerHTML+=firefoxHtml,document.getElementById("firefoxWarningthing").addEventListener("click",()=>{document.getElementById("firefoxWarning").showModal()}));export{auth,db,realtime,analytics,storage,ONLINE_TEXT,OFFLINE_TEXT,KIT_COVER_LOCATION,DEVELOPER_ALLOW_LIST,MODERATOR_ALLOW_LIST,adjectiveWords,animals,forceOffline,FirebaseHelper,HOSTING_VERSION,signInAnonymously,firefoxAgent,reauthenticateWithPopup,Timestamp,hasBluekidPlus,EmailAuthProvider,reauthenticateWithCredential,functions,httpsCallable,storageref,startAfter,uploadBytes,deleteObject,listAll,onAuthStateChanged,GoogleAuthProvider,signInWithPopup,signInWithEmailAndPassword,sendPasswordResetEmail,getDownloadURL,doc,getDoc,ref,onDisconnect,signOut,getDocs,updateDoc,deleteDoc,collection,query,where,limit,orderBy,setDoc,deleteUser,updateEmail,addDoc,realtimeSet,realtimeUpdate,realtimeGet,realtimeRemove,onValue};
+`;
+
+let userAgentString = navigator.userAgent;
+let firefoxAgent = userAgentString.indexOf("Firefox") > -1; 
+
+if (firefoxAgent) {
+    document.body.innerHTML += firefoxHtml;
+    document.getElementById("firefoxWarningthing").addEventListener("click", () => {
+        document.getElementById("firefoxWarning").showModal();
+    });
+}
+
+// Not added: 
+export { HOSTING_VERSION, signInAnonymously, firefoxAgent, reauthenticateWithPopup, Timestamp, hasBluekidPlus, EmailAuthProvider, reauthenticateWithCredential, functions, httpsCallable, storageref, startAfter, uploadBytes, deleteObject, listAll, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail, getDownloadURL, doc, getDoc, ref, onDisconnect, signOut, getDocs, updateDoc, deleteDoc, collection, query, where, limit, orderBy, setDoc, deleteUser, updateEmail, addDoc, realtimeSet, realtimeUpdate, realtimeGet, realtimeRemove, onValue };
